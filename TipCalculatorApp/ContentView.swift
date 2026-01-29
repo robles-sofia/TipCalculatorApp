1f8e28c6e23b199b16d293ec4a26d0407bb7686a
Sofia Robles <robles@wustl.edu>
2026-01-28 20:36:32 -0600

    Completed Assignment 1: Tip Calculator App

--- TipCalculatorApp/ContentView.swift
+++ TipCalculatorApp/ContentView.swift
@@ -1,22 +1,80 @@
-//
-//  ContentView.swift
-//  TipCalculatorApp
-//
-//  Created by Sofia Robles on 1/28/26.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount: Double = 0
+    @State private var tipPercentage: Double = 0
+    @State private var numberOfPeople: Double = 1
+    @State private var showResults: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 25) {
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            VStack(alignment: .leading) {
+                Text("Bill Amount")
+                
+                Slider(value: $billAmount, in: 0...500, step: 1)
+                
+                Text("\(billAmount, format: .currency(code: "USD"))")
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Tip Percentage")
+                
+                Slider(value: $tipPercentage, in: 0...30, step: 1)
+                
+                Text("\(Int(tipPercentage))%")
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Number of People")
+                
+                Slider(value: $numberOfPeople, in: 1...20, step: 1)
+                
+                Text("\(Int(numberOfPeople))")
+            }
+            
+            if showResults {
+                VStack(spacing: 10) {
+                    Text("Tip: \(tipAmount, format: .currency(code: "USD"))")
+                    Text("Total: \(totalAmount, format: .currency(code: "USD"))")
+                    Text("Per Person: \(amountPerPerson, format: .currency(code: "USD"))")
+                }
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(12)
+                .shadow(radius: 5)
+            }
+            
+            Button {
+                showResults.toggle()
+            } label: {
+                Text(showResults ? "Hide Results" : "Calculate")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(showResults ? Color.red : Color.blue)
+                    .cornerRadius(12)
+            }
+            
+            Spacer()
         }
         .padding()
     }
+    
+    var tipAmount: Double {
+        billAmount * (tipPercentage / 100)
+    }
+    var totalAmount: Double {
+        billAmount + tipAmount
+    }
+    var amountPerPerson: Double {
+        totalAmount / max(numberOfPeople, 1)
+    }
+    
 }
 
 #Preview {
